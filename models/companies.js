5f7f0f6c208d0244d6779e3fa851161b7921f552
R Brian Redd <me@rbrianredd.com>
2018-07-22 03:35:39 +0000

    Created Orders.js, minor Schema changes

--- models/companies.js
+++ models/companies.js
@@ -1,22 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
-require("mongoose-currency").loadType(mongoose);
-var Currency = mongoose.Types.Currency;
-
-var imageSchema = new Schema({
-    assetpath: {
-        type: String
-    },
-    caption: {
-        type: String
-    },
-    valid: {
-        type: Boolean
-    }
-}, {
-    timestamps: true
-})
+const orderSchema = require("./orders");
 
 var deviceSchema = new Schema({
     deviceModel: {
@@ -35,116 +19,6 @@ var deviceSchema = new Schema({
         type: String
     }
 })
-
-var orderSchema = new Schema({
-    orderId: {
-        type: String,
-        required: true
-    },
-    firstName: {
-        type: String,
-        required: true
-    },
-    lastName: {
-        type: String,
-        required: true
-    },
-    addressStreet: {
-        type: String,
-        required: true
-    },
-    addressCity: {
-        type: String,
-        required: true
-    },
-    addressState: {
-        type: String,
-        required: true
-    },
-    addressZip: {
-        type: Number,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    phone: {
-        type: String,
-        required: true
-    },
-    images: [imageSchema],
-    issue: {
-        type: String,
-        required: true
-    },
-    issueDetail: {
-        type: String,
-        default: ''
-    },
-    repairLoc: {
-        type: String,
-        required: true
-    },
-    repairCost: {
-        type: Currency,
-        default: 0
-    },
-    repairPaid: {
-        type: Boolean
-    },
-    shipCost: {
-        type: Currency,
-        default: 0
-    },
-    shipPaid: {
-        type: Boolean
-    },
-    estRepair: {
-        type: Date,
-        required: true
-    },
-    shopLoc: {
-        type: String,
-        required: true
-    },
-    notes: {
-        type: String,
-        default: ''
-    },
-    uploaded: {
-        type: Boolean
-    },
-    uploadedDateTime: {
-        type: Date
-    },
-    accepted: {
-        type: Boolean
-    },
-    acceptedDateTime: {
-        type: Date
-    },
-    shippedOffsite: {
-        type: Boolean
-    },
-    shippedDateTime: {
-        type: Date
-    },
-    completed: {
-        type: Boolean
-    },
-    completedDateTime: {
-        type: Date
-    },
-    delivered: {
-        type: Boolean
-    },
-    deliveredDateTime: {
-        type: Date
-    }
-}, {
-    timestamps: true
-});
 
 var companySchema = new Schema({
     name: {
@@ -226,7 +100,7 @@ var companySchema = new Schema({
         default: 5
     },
     devices: [deviceSchema],
-    orders: [orderSchema]
+    orders: [orderSchema.schema]
 }, {
     timestamps: true
 });
